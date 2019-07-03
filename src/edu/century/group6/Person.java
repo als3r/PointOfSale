9fdeadbc3cc3e597dc8374043abd1542761f7e84
Alexandr Sergeyev <ns1418cz@my.century.edu>
2019-07-02 20:14:39 -0500

    initial commit2

---
+++ src/edu/century/group6/Person.java
@@ -0,0 +1,156 @@
+package edu.century.group6;
+
+/**
+ * PointOfSale
+ * 
+ * Definition of person class
+ * 
+ * Class: CSCI 1082-90 - Object Oriented Programming
+ * Instructor: Matthew Sanders
+ * Assignment: Group Assignment
+ * Date: July 02, 2019
+ * Due:  Jule 16, 2019
+ * 
+ * Group 6
+ * @author Anan Gedefa <pv2824bf@my.century.edu>
+ * @author Evelline Samson <cj5673li@my.century.edu> 
+ * @author Alexandr Sergeyev <ns1418cz@my.century.edu>
+ */
+public class Person {
+	
+	/**
+	 * First name
+	 */
+	private String firstName;
+	
+	/**
+	 * Last name
+	 */
+	private String lastName;
+	
+	/**
+	 * Address
+	 */
+	private String address;
+	
+	/**
+	 * City
+	 */
+	private String city;
+	
+	/**
+	 * State
+	 */
+	private String state;
+	
+	/**
+	 * Get First name
+	 * 
+	 * @return
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	/**
+	 * @param firstName
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	/**
+	 * @return
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+
+	/**
+	 * @param lastName
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	/**
+	 * @return
+	 */
+	public String getAddress() {
+		return address;
+	}
+
+	/**
+	 * @param address
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	/**
+	 * @return
+	 */
+	public String getCity() {
+		return city;
+	}
+
+	/**
+	 * @param city
+	 */
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	/**
+	 * @return
+	 */
+	public String getState() {
+		return state;
+	}
+
+	/**
+	 * @param state
+	 */
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	/**
+	 * @return
+	 */
+	public String getZip() {
+		return zip;
+	}
+
+	/**
+	 * @param zip
+	 */
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+	/**
+	 * @return
+	 */
+	public String getPhone() {
+		return phone;
+	}
+
+	/**
+	 * @param phone
+	 */
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	/**
+	 * 
+	 */
+	private String zip;
+	/**
+	 * 
+	 */
+	private String phone;
+	
+	
+}
