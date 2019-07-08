b21a28ec8150b94fe4b60d4563d55dc22780c59e
Alex <sergeyev.alex@gmail.com>
2019-07-08 14:08:02 -0500

    update order class

--- src/edu/century/group6/Driver.java
+++ src/edu/century/group6/Driver.java
@@ -1,5 +1,7 @@
 package edu.century.group6;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * PointOfSale
  * 
@@ -19,11 +21,67 @@ package edu.century.group6;
 public class Driver {
 
 	public static void main(String[] args) {
-		System.out.println("test");
 		
-		Customer customer = new Customer();
-		Employee employee = new Employee();
+//		Customer customer = new Customer();
+//		Employee employee = new Employee();
+//		System.out.println("test");
+//		System.out.println(customer.toString());
+//		System.out.println(employee.toString());
 		
+		
+		echo("***** PointOfSale tests *****");
+		echo("");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		// test order track time functions
+		Order order1 = new Order();
+		echo("***** Test Order Track Time *****");
+		echo("Create empty order1");
+		
+		try {
+			// change order status to order received
+			order1.orderStatusReceived();
+			echo("Order Received Time: " + order1.getOrderReceivedTimeFormatted());
+			
+			// wait and change to cooked
+			Thread.sleep(1500);
+			order1.orderStatusCooked();
+			echo("Order Cooked Time: " + order1.getOrderCookTimeFormatted());
+			
+			// wait and change to delivered
+			Thread.sleep(2300);
+			order1.orderStatusDelivered();
+			echo("Order Delivered Time: " + order1.getOrderDeliveryTimeFormatted());
+			
+			// wait and change to completed
+			Thread.sleep(4500);
+			order1.orderStatusCompleted();
+			echo("Order Completed Time: " + order1.getOrderCompletedTimeFormatted());
+			
+			// get total order time
+			echo("Total Order Time (full): " + order1.getOrderTotalTimeFormatted());
+			echo("Total Order Time (in sec): " + order1.getOrderTotalTimeSeconds());
+		
+		} catch (InterruptedException ie) {
+			echo("Error. Cannot use wait");
+		}
+		echo("***** // END Test Order Track Time *****");
+		echo("");
+		
+		
+		
+	}
+	
+	
+	public static void echo (String string) {
+		System.out.println(string);
 	}
 
 }
--- src/edu/century/group6/Order.java
+++ src/edu/century/group6/Order.java
@@ -1,10 +1,25 @@
 package edu.century.group6;
 
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * PointOfSale
  * 
  * Definition of order class
  * Stores information about order
+ * 
+ * Order has 4 step/statuses:
+ * - new order
+ * - cooking
+ * - delivery
+ * - completed
+ * 
+ * each time order status is changed we update time for each step/status
+ * to be able to track order completion time
+ * 
  * 
  * Class: CSCI 1082-90 - Object Oriented Programming
  * Instructor: Matthew Sanders
@@ -19,4 +34,448 @@ package edu.century.group6;
  */
 public class Order {
 
+	/**
+	 * Order Number
+	 */
+	private String orderNumber;
+
+	/**
+	 * Max Items in Order
+	 */
+	private static final int MAX_ORDER_ITEMS = 100;
+	
+	/**
+	 * Stores order Items
+	 */
+	private OrderItem[] orderItems;
+	
+	/**
+	 * Number of items in the order
+	 */
+	private int numItems;
+	
+	/**
+	 * Order Comment
+	 */
+	private String comment;
+	
+	/**
+	 * Customer
+	 */
+	private Customer customer;
+	
+	/**
+	 * Employee who created order
+	 */
+	private Employee employee;
+	
+	/**
+	 * Delivery person
+	 */
+	private Employee delivery;
+	
+	/**
+	 * Delivery address
+	 */
+	private String deliveryAddress;
+	
+	/**
+	 * Time needed to deliver order
+	 */
+	private Date deliveryTime;
+	
+	/**
+	 * Time when order was received
+	 */
+	private Date orderReceivedTime;
+	
+	/**
+	 * Time when order was cooked
+	 */
+	private Date orderCookTime;
+	
+	/**
+	 * Time when order was delivered
+	 */
+	private Date orderDeliveryTime;
+	
+	/**
+	 * Time when order was completed
+	 */
+	private Date orderCompletedTime;
+	
+	/**
+	 * Order total time, in seconds
+	 */
+	private long orderTotalTime;
+
+	/**
+	 * Order Status
+	 * 
+	 */
+	private enum ORDER_STATUSES {
+		ORDER_STATUS_RECEIEVED, 
+		ORDER_STATUS_PREPARED, 
+		ORDER_STATUS_DELIVERED, 
+		ORDER_STATUS_COMPLETED
+	};
+	
+	/**
+	 * order Status from orderStatuses
+	 */
+	private ORDER_STATUSES orderStatus;
+	
+	/**
+	 * Order Created Format
+	 */
+	public static String orderDatePattern = "H:m:s MM/dd/yyyy";
+	
+	
+	
+	
+	
+	/*
+	 * Getter and Setters
+	 */
+	
+	public String getOrderNumber() {
+		return orderNumber;
+	}
+
+	public void setOrderNumber(String orderNumber) {
+		this.orderNumber = orderNumber;
+	}
+	
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
+	public Employee getEmployee() {
+		return employee;
+	}
+
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
+	}
+
+	public Employee getDelivery() {
+		return delivery;
+	}
+
+	public void setDelivery(Employee delivery) {
+		this.delivery = delivery;
+	}
+
+	public String getDeliveryAddress() {
+		return deliveryAddress;
+	}
+
+	public void setDeliveryAddress(String deliveryAddress) {
+		this.deliveryAddress = deliveryAddress;
+	}
+	
+	/**
+	 * Return order received time (Step 1)
+	 * @return Date orderReceivedTime
+	 */
+	public Date getOrderReceivedTime() {
+		return orderReceivedTime;
+	}
+
+	/**
+	 * Return formatted order received time (Step 1)
+	 * @return String orderDeliveryTime
+	 */
+	public String getOrderReceivedTimeFormatted() {
+		return new SimpleDateFormat(orderDatePattern).format(getOrderReceivedTime());
+	}
+	
+	/**
+	 * Modifies orderReceivedTime
+	 * Sets time to current time
+	 */
+	public void setOrderReceivedTime() {
+		this.orderReceivedTime = new Date();
+	}
+
+	/**
+	 * Modifies orderReceivedTime
+	 * @param orderReceivedTime
+	 */
+	public void setOrderReceivedTime(Date orderReceivedTime) {
+		this.orderReceivedTime = orderReceivedTime;
+	}
+	
+	/**
+	 * Return order cook time (Step 2)
+	 * @return Date orderCookTime
+	 */
+	public Date getOrderCookTime() {
+		return orderCookTime;
+	}
+
+	/**
+	 * Return formatted order cook time (Step 2)
+	 * @return String orderDeliveryTime
+	 */
+	public String getOrderCookTimeFormatted() {
+		return new SimpleDateFormat(orderDatePattern).format(getOrderCookTime());
+	}
+
+	/**
+	 * Modifies orderCookTime
+	 * Sets time to current time
+	 */
+	public void setOrderCookTime() {
+		this.orderCookTime = new Date();
+	}
+	
+	/**
+	 * Modifies orderCookTime
+	 * @param orderCookTime
+	 */
+	public void setOrderCookTime(Date orderCookTime) {
+		this.orderCookTime = orderCookTime;
+	}
+	
+	/**
+	 * Return order delivery time (Step 3)
+	 * @return Date orderDeliveryTime
+	 */
+	public Date getOrderDeliveryTime() {
+		return orderDeliveryTime;
+	}
+
+	/**
+	 * Return formatted order delivery time (Step 3)
+	 * @return String orderDeliveryTime
+	 */
+	public String getOrderDeliveryTimeFormatted() {
+		return new SimpleDateFormat(orderDatePattern).format(getOrderDeliveryTime());
+	}
+
+	/**
+	 * Modifies orderDeliveryTime
+	 * Sets time to current time
+	 */
+	public void setOrderDeliveryTime() {
+		this.orderDeliveryTime = new Date();
+	}
+
+	/**
+	 * Modifies orderDeliveryTime
+	 * @param orderCookTime
+	 */
+	public void setOrderDeliveryTime(Date orderDeliveryTime) {
+		this.orderDeliveryTime = new Date();
+	}
+	
+	/**
+	 * Return order completed time (Step 4)
+	 * @return Date orderCompletedTime
+	 */
+	public Date getOrderCompletedTime() {
+		return orderCompletedTime;
+	}
+
+	/**
+	 * Return formatted order total time (Step 4)
+	 * @return String orderTotalTime
+	 */
+	public String getOrderCompletedTimeFormatted() {
+		return new SimpleDateFormat(orderDatePattern).format(getOrderCompletedTime());
+	}
+
+	/**
+	 * Modifies orderCompletedTime
+	 * Sets time to current time
+	 */
+	public void setOrderCompletedTime() {
+		this.orderCompletedTime = new Date();
+	}
+
+	/**
+	 * Modifies orderCompletedTime
+	 * @param orderCompletedTime
+	 */
+	public void setOrderCompletedTime(Date orderCompletedTime) {
+		this.orderCompletedTime = orderCompletedTime;
+	}
+
+	/**
+	 * Return order total time
+	 * @return long orderTotalTime
+	 */
+	public long getOrderTotalTime() {
+		return orderTotalTime;
+	}
+	
+	public long getOrderTotalTimeSeconds() {
+		return orderTotalTime;
+	}
+
+	/**
+	 * Return formatted order total time (0m 0s)
+	 * @return String orderTotalTime
+	 */
+	public String getOrderTotalTimeFormatted() {
+		
+		String message = "";
+		long minutes = 0;
+		long seconds = 0;
+		
+		minutes = getOrderTotalTime() / 60;
+		seconds = getOrderTotalTime() % 60;
+		
+		if(minutes > 0) {
+			message += minutes + "m ";
+		}
+		
+		message += seconds + "s";
+	
+		return message;
+	}
+
+	public void setOrderTotalTime() {
+		long miliseconds = (getOrderCompletedTime().getTime() - getOrderReceivedTime().getTime());
+		this.orderTotalTime = miliseconds/1000;
+	}
+	
+	public ORDER_STATUSES getOrderStatus() {
+		return orderStatus;
+	}
+
+	public void setOrderStatus(ORDER_STATUSES orderStatus) {
+		this.orderStatus = orderStatus;
+	}
+
+
+
+	/*
+	 * Default constructor for order
+	 * 
+	 * Construct an instance with default values
+	 */
+	Order(){
+		orderStatusReceived();	
+	}
+	
+	public boolean orderStatusReceived() {
+		this.orderItems = new OrderItem[MAX_ORDER_ITEMS];
+		setOrderStatus(ORDER_STATUSES.ORDER_STATUS_RECEIEVED);
+		setOrderReceivedTime();
+		return true;
+	}
+	
+	public boolean orderStatusCooked() {
+		setOrderStatus(ORDER_STATUSES.ORDER_STATUS_PREPARED);
+		setOrderCookTime();
+		return true;
+	}
+	
+	public boolean orderStatusDelivered() {
+		setOrderStatus(ORDER_STATUSES.ORDER_STATUS_DELIVERED);
+		setOrderDeliveryTime();
+		return true;
+	}
+	
+	public boolean orderStatusCompleted() {
+		setOrderStatus(ORDER_STATUSES.ORDER_STATUS_COMPLETED);
+		setOrderCompletedTime();
+		setOrderTotalTime();
+		return true;
+	}
+	
+	public Order addOrderItem(OrderItem orderItem) {
+		for (int i = 0; i < orderItems.length; i++) {
+			if (orderItems[i] == null) {
+				orderItems[i] = orderItem;
+				break;
+			}
+		}
+		return this;
+	}
+	
+	public void removeOrderItem() {
+		
+	}
+
+
+
+	public OrderItem[] getOrderItems() {
+		return orderItems;
+	}
+
+
+
+	public void setOrderItems(OrderItem[] orderItems) {
+		this.orderItems = orderItems;
+	}
+
+
+
+	public int getNumItems() {
+		return numItems;
+	}
+
+
+
+	public void setNumItems(int numItems) {
+		this.numItems = numItems;
+	}
+	
+	
+	public void calcSubTotal() {
+		
+	}
+	
+	public void calcTax() {
+		
+	}
+	
+	public void calcTotal() {
+		
+	}
+	
+	
+	public void printOrder() {
+		
+	}
+	
+	
+	public void sendOrder() {
+		
+	}
+	
+	
+	public void calcOrderTime() {
+		
+	}
+	
+	public void calcWaitTime() {
+		
+	}
+	
+	/**
+	 * Overriding method definition in order to be more verbose in output
+	 * 
+	 * @return the formatted string of reservation data
+	 */
+	@Override
+	public String toString() {
+		String message = "";
+		return message;
+	}
 }
