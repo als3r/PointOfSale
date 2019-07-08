d10a9ff90e89b533c287d2fdc84b5ef0acdec382
AnanGedefa <52475188+AnanGedefa@users.noreply.github.com>
2019-07-07 20:26:29 -0500

    Update SideItem.java

--- src/edu/century/group6/SideItem.java
+++ src/edu/century/group6/SideItem.java
@@ -20,4 +20,98 @@ package edu.century.group6;
  */
 public class SideItem extends orderItem{
 
+   private String drink;
+  private String fries;
+  
+  
+  /**
+  * Default constractor initialized to null and 0
+  *
+  */
+  public SideItem(){
+    
+    this.drink = "";
+    this.fries = ""'
+    
+ }
+  
+ public SideItem (String drinks, String fries){
+    
+    this.drink = drink;
+    this.fries = fries;
+   
+ }
+  
+  
+  /**
+  * mutator and accessor methods to get and set side items
+  * 
+  *
+  */
+ 
+	public String getDrink()
+	{
+		return drink;
+	}
+
+	public void setDrink(String drink)
+	{
+		this.drink = drink;
+	}
+
+	public String getFries()
+	{
+		return fries;
+	}
+
+	public void setFries(int fries)
+	{
+		this.fries = fries;
+	}
+  
+  
+  
+  /**
+  * price calculation
+  *
+  *
+  *
+  */
+  
+  
+  
+  
+public double calcCost()
+	{		
+		if(drink.equalsIgnoreCase("coke"))
+		{
+			return 1.00;
+		}
+		else if(drink.equalsIgnoreCase("water"))
+		{
+			return 2.00;
+		}
+		else if(drink.equalsIgnoreCase("beer"))
+		{
+			return 3.99;
+		}
+		else
+		{
+			return 0.0;
+		}
+	}
+  
+  /**
+  * @overide
+  *
+  */
+
+	public String toString()
+	{
+		return "Side item: " + drink + fries; 
+				
+	}
+}
+  
+  
 }
