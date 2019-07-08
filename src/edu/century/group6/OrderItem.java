d73b9345f1cac802f796f841eac716ab55b7be37
AnanGedefa <52475188+AnanGedefa@users.noreply.github.com>
2019-07-07 19:20:30 -0500

    Update OrderItem.java

--- src/edu/century/group6/OrderItem.java
+++ src/edu/century/group6/OrderItem.java
@@ -1,5 +1,49 @@
 package edu.century.group6;
 
+/**
+* contains instance variable to hold
+* Order price
+* Unique order ID 
+* Description of order
+*
+*
+*/
 public class OrderItem {
 
+  private Price price;
+  String id;
+  
+  /**
+  *
+  *mutotor and accessor methods for price 
+  *and id
+  *
+  */
+  Public void setPrice(Price price){
+    this.price=price;
+  }
+  
+  public Price getPrice(){
+    return price;
+  }
+  
+  public void setID(String id){
+    this.id=id;
+  }
+  
+  public String getID(){
+    return id;
+  }
+  
+  /**
+  *
+  *@overide
+  *
+  */
+  public String toString(){
+    
+    return 
+    
+  }
+  
 }
